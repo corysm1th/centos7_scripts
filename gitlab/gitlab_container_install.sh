002bd59c6ccfde64c1c7c44b326d52a163f004ed
Cory Smith <smith.cory@gmail.com>
2018-01-27 22:08:02 -0800

    Fixed variable expansion

--- gitlab/gitlab_container_install.sh
+++ gitlab/gitlab_container_install.sh
@@ -4,28 +4,28 @@ if [ -z "$3" ]; then
     exit 1
 fi
 
-if [ ! -f /data/gitlab/ssl/$3.crt ]; then
-    echo "Missing /data/gitlab/ssl/$3.crt"
+if [ ! -f /data/gitlab/ssl/"$3".crt ]; then
+    echo "Missing /data/gitlab/ssl/"$3".crt"
     mkdir -p /data/gitlab/ssl
-    touch /data/gitlab/ssl/$3.crt
+    touch /data/gitlab/ssl/"$3".crt
     exit 1
 fi
 
-if [ ! -f /data/gitlab/ssl/$3.key ]; then
-    echo "Missing /data/gitlab/ssl/$3.key"
-    touch /data/gitlab/ssl/$3.key
+if [ ! -f /data/gitlab/ssl/"$3".key ]; then
+    echo "Missing /data/gitlab/ssl/"$3".key"
+    touch /data/gitlab/ssl/"$3".key
     exit 1
 fi
 
 iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 443 -j ACCEPT
-iptables -I FORWARD -d $2 -j ACCEPT
+iptables -I FORWARD -d "$2" -j ACCEPT
 iptables-save | awk '!COMMIT||!x[$0]++' | iptables-restore
 
 mkdir -p /data/gitlab/{config,logs,data}
 
-docker run -d --net $1 --ip $2 \
-    --name $3 \
-    --env GITLAB_OMNIBUS_CONFIG="external_url 'https://\${3}/';" \
+docker run -d --net "$1" --ip "$2" \
+    --name "$3" \
+    --env GITLAB_OMNIBUS_CONFIG="external_url \"https://"${URL}"\";" \
     --name gitlab \
     --restart always \
     --volume /data/gitlab/config:/etc/gitlab:Z \
