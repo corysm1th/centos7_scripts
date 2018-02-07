5197bdaf3aa814e0f8777a87c3448fb216fdf473
Cory Smith <smith.cory@gmail.com>
2018-02-06 18:18:44 -0800

    Specify 17.03.2 for kubernetes support.

--- docker/docker_install.sh
+++ docker/docker_install.sh
@@ -2,7 +2,7 @@
 
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install -y docker-ce
+yum install -y docker-ce-17.03.2
 
 systemctl enable docker
 systemctl start docker
