7fe24452941c863b6c827d72a2ae55560c1041e0
Cory Smith <smith.cory@gmail.com>
2018-02-06 19:02:32 -0800

    Update docker_install.sh

--- docker/docker_install.sh
+++ docker/docker_install.sh
@@ -9,3 +9,5 @@ systemctl start docker
 
 curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` \
   -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
